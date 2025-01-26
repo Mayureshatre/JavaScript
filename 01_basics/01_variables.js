110eedd9f3cb570ae84a35142468a4a99368f54b
Mayureshatre <atremayuresh51@gmail.com>
2025-01-26 07:34:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 11223344
+let emailId = "mayuresh@gmail.com"
+var password = "123456" //var keyword is prefered to use because it does not follow the scope rules of a variable
+accountCity = "Burhanpur"
+let accountState; // semicolon does not affect the code
+
+// accountId = 555      const keyword doesnot allow changes in the value 
+
+console.log(accountId);
+console.table([accountId, emailId, password, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"Use Strict"; //to use newer version of JS
+
+// alert(3+3)   it works in the terminal of browser, but not in node JS
+
+
+//code readability should be high like below
+console.log(3+3);
+
+console.log("Mayuresh");
+// not like this,it will work but readability is poor
+console.log(3
+    +3);
+
+let Name = "Mayuresh" // string in double quote
+let age = 18 // number range about 2 to the opwer 53
+let state; // undefined datatype, value is not assigned
+let a=null; //null datatype, standalone value value is assigned but it is null 
+    //bigint for number greater than 2 to the power of 53
+    //boolean , true/false
+    //symbol ,unique
+
+//object
+console.log(typeof null); // null is an object!!
+console.log(typeof undefined);
