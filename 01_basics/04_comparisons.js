2dd678bd00af4c9ea8c45feb78d2b3cbfc7ccf98
Mayureshatre <atremayuresh51@gmail.com>
2025-02-04 16:57:35 +0000

    comparison operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+
+// console.log("2" > 1); // true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);  false
+// console.log(null == 0);  false
+// console.log(null >= 0);  true
+
+//equality operator works differently from comparison operator, comparison operator treats null as a zero.
+
+// console.log(undefined == 0);  false
+// console.log(undefined >= 0);  false
+// console.log(undefined <= 0);  false
+
+// === is used for strict check datatype is also checked
+
+// console.log("2" == 2);  true
+// console.log("2" === 2); false
