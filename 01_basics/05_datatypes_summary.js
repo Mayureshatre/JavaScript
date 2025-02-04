3d844119f4c93198a3f1e93c310ba917fadb3535
Mayureshatre <atremayuresh51@gmail.com>
2025-02-04 17:25:29 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,18 @@
+// primitive datatypes
+// string, number, boolean, null, undefined, symbol, BigInt
+
+// reference (non-primitive datatypes)
+// array, objects, functions
+
+// *js has dynamically typed language*
+
+let Id = Symbol('123')
+let anotherId = Symbol('123')
+// both are still unique
+//console.log(Id === anotherId);  false
+
+/* typeof of undefined is undefined
+   typeof null is object
+   typeof function is objectfunction
+   typeof BigInt is undefined
+ */
