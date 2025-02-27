05c7915cda4b6f7caca8efd31caa57e0d4d2dbf2
Mayureshatre <atremayuresh51@gmail.com>
2025-02-27 16:57:16 +0000

    objects in JS

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,43 @@
+// const tinderUser = new Object() --> singleton object 
+const tinderUser = {}  // --> non singleton object
+
+tinderUser.Id = "123abc"
+tinderUser.Name = "Sam"
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUSer = {
+    email: "someone@gmail.com",
+    fullNmae: {
+        firstName: "Mayuresh",
+        lastName: "Atre"
+    },
+    age: 18
+}
+console.log(regularUSer.fullNmae.firstName);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+const obj3 = {
+    5: "e",
+    6:"f"
+}
+// const obj4 = { obj1, obj2}
+// const obj4 = Object.assign({}, obj1, obj2, obj3) // the first object act as a target object for all objects
+
+const obj4  = {...obj1, ...obj2, ...obj3}
+console.log(obj4);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
