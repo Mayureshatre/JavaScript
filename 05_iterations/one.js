14d9f8f12070a98ac0e99dfaf088a265a4d02ae6
Mayureshatre <atremayuresh51@gmail.com>
2025-03-30 12:37:24 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 10 ; i++) {
+    const element = i;
+    // console.log(element);
+}
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`outer loop ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner lopp ${j} and outer loop ${i}`);
+        //  console.log(`${i}  X  ${j} =  ${i*j}`);
+         
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    // console.log(element);
+    
+}
