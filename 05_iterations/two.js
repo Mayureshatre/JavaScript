1c0ec8753294f38a4ae9135c7f4927e5c2532519
Mayureshatre <atremayuresh51@gmail.com>
2025-03-31 06:19:19 +0000

    while do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index = index+2;
+}
+
+let MyArray = ["flash", "Batman", "Superman", "Ironman", "Spiderman"]
+let arr = 0
+while(arr<MyArray.length){
+    console.log(MyArray[arr]);
+    arr++;
+}
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
