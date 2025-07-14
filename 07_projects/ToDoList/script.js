f3a69cda7811b59419590e14273ea67bd46d72c5
Mayureshatre <atremayuresh51@gmail.com>
2025-07-14 16:54:42 +0000

    To Do List

--- 07_projects/ToDoList/script.js
+++ 07_projects/ToDoList/script.js
@@ -28,25 +28,74 @@ function addTask() {
     priority: priorityInput.value,
   };
   arrTasks.push(task);
+  taskInput.value = "";
+
   displayTask();
 }
 
 function displayTask() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  if (arrTasks.length === 0) {
+    taskList.innerHTML = "<p>No tasks available</p>";
+    return;
+  }
+
   arrTasks.forEach((obj) => {
-    if (arrTasks.length === 0) {
-            const taskList = document.getElementById("taskList");
-            taskList.innerHTML = "<p>No tasks available</p>";
-        }
-    const li = document.createElement("li");
-    const deleteButton = document.createElement("button");
-    li.innerHTML = `<span class="task&priority">${obj.task} - (${obj.priority})</span><button class="delete"><img src="https://tse4.mm.bing.net/th/id/OIP.OLhf-Opp-n2b1r8coDAjRwHaHa?pid=Api&P=0&w=300&h=300" alt="Delete"></button>`;
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    let editButton = document.createElement("button");
+
+    li.innerHTML = `<span class="task&priority">${obj.task} - (${obj.priority})</span>`;
+    deleteButton.classList = "delete";
+    deleteButton.innerHTML = `<img src="https://tse4.mm.bing.net/th/id/OIP.OLhf-Opp-n2b1r8coDAjRwHaHa?pid=Api&P=0&w=300&h=300" alt="Delete">`;
+    editButton.classList = "edit";
+    editButton.innerHTML = `<img src="https://icon-library.com/images/edit-icon/edit-icon-0.jpg" alt="Edit">`;
+
+    li.appendChild(editButton)
+    li.appendChild(deleteButton)
+    taskList.appendChild(li);
+
     deleteButton.addEventListener("click", (e) => {
       e.preventDefault();
       li.remove();
-      arrTasks = arrTasks.filter(
-        (kaam) => kaam.task !== obj.task || kaam.priority !== obj.priority
-      );
+      // arrTasks = arrTasks.filter(
+      //   (kaam) => kaam.task !== obj.task || kaam.priority !== obj.priority
+      // );
+      arrTasks.splice(arrTasks.indexOf(obj), 1)
+      displayTask();
     });
   });
 }
-function deleteTask() {}
+function deleteAllTask() {}
+
+function sortTask(){
+  const priorityMap = {
+    High: 1,
+    Medium: 2,
+    Low: 3
+  }
+  arrTasks.sort((a, b) => {
+    return priorityMap[a.priority] - priorityMap[b.priority]
+  })
+  displayTask()
+}
+
+const allTasks = document.querySelectorAll('li')
+const arrAllTasks = Array.from(allTasks);
+
+arrAllTasks.forEach((taskitem) => {
+  editButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(taskitem);
+    editTask(arrAllTasks.indexOf(taskitem));
+  })
+})
+
+function editTask(index){
+  const kaam = arrTasks(index)
+  taskInput.value = kamm.task;
+  priorityInput.value = kaam.priority;
+  arrTasks.splice(index,1)
+  displayTask();
+}
